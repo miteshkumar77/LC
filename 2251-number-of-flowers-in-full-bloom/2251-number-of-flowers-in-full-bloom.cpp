21c984fe1ee279954d2fb90e9bf60af95ed330c2
miteshkumar77 <48740739+miteshkumar77@users.noreply.github.com>
2022-04-24 08:24:41 -0700

    Time: 495 ms (20.00%), Space: 93.9 MB (20.00%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    enum Ord : short { Start, Person, End };
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& persons) {
-        vector<pair<int, Ord>> ev;
-        ev.reserve(flowers.size() + persons.size());
-        unordered_map<int, vector<int>> pos;
-        for (int i = 0; i < persons.size(); ++i) {
-            pos[persons[i]].push_back(i);
+        vector<array<int, 3>> evs;
+        for (auto const& f : flowers) {
+            evs.push_back({f[0], 0, 1});
+            evs.push_back({f[1], 2, 1});
         }
-        for (auto const& v : flowers) {
-            ev.push_back({v[0], Ord::Start});
-            ev.push_back({v[1], Ord::End});
-        }
-        set<pair<int, Ord>> cmp;
-        for (auto const& p : persons) {
-            cmp.insert({p, Ord::Person});
-        }
-        
-        for (auto p : cmp)
-            ev.push_back(p);
-        sort(ev.begin(), ev.end());
-        
-        vector<int> ans(persons.size(), 0);
-        int lvl = 0;
-        
-        for (auto [cp, T] : ev) {
+        for (int i = 0; i < persons.size(); ++i)
+            evs.push_back({persons[i], 1, i});
+        vector<int> ans(persons.size());
+        sort(evs.begin(), evs.end());
+        int lvl{0};
+        for (auto [pos, T, i] : evs) {
             switch (T) {
-                case Ord::Start:
+                case 0:
                     ++lvl;
                     break;
-                case Ord::Person:
-                    for (int p : pos[cp])
-                        ans[p] = lvl;
+                case 1:
+                    ans[i] = lvl;
                     break;
-                case Ord::End:
+                case 2:
                     --lvl;
                     break;
             }
