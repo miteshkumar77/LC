ad5da496548d6318e18880c6569048e126df2fea
miteshkumar77 <48740739+miteshkumar77@users.noreply.github.com>
2022-05-07 16:26:50 -0700

    Time: 280 ms (6.30%), Space: 54.1 MB (10.84%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void erase(int num, map<int,int>& mp) {
+        auto it = mp.find(num);
+        --(it -> second);
+        if (it -> second == 0) {
+            mp.erase(it);
+        }
+    }
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        map<int,int> suff;
+        
+        for (int i = 1; i < n; ++i) {
+            ++suff[nums[i]];
+        }
+        
+        int minsf = nums[0];
+        for (int i = 1; i < n; ++i) {
+            auto it = suff.find(nums[i]);
+            if (it != suff.begin()) {
+                it = prev(it);
+                if (it -> first > minsf) {
+                    return true;
+                }
+            }
+            minsf = min(nums[i], minsf);
+            erase(nums[i], suff);
+        }
+        return false;
+    }
+};
