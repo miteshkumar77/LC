ce6901e726ee57805dca19640b47dbf69d28dcb3
miteshkumar77 <48740739+miteshkumar77@users.noreply.github.com>
2022-08-20 14:20:20 -0400

    Time: 1736 ms (8.17%), Space: 63.2 MB (32.67%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // find the largest index i in nums such that nums[i] < t
+    // assuming nums is sorted in ascending order
+    // return -1 if no such index exists
     int find(vector<int> const& nums, int l, int r, int t) {
         int ans = -1;
         int mid;
@@ -25,8 +28,8 @@ public:
         int n = arr1.size();
         int m = arr2.size();
         vector<vector<array<int, 2>>> dp(n, vector<array<int, 2>>(m, {M, M}));
-        vector<array<int, 2>> pres(m);
-        vector<array<int, 2>> pres2(m);
+        vector<int> pres(m);
+        vector<int> pres2(m);
         int ans = M;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
@@ -43,28 +46,16 @@ public:
                     int kp = find(arr2, 0, j-1, arr2[j]);
                     int k = find(arr2, 0, j, arr1[i]);
                     if (kp != -1) {
-                        dp[i][j][1] = min(dp[i][j][1], pres2[kp][1] + 1);
+                        dp[i][j][1] = min(dp[i][j][1], pres2[kp] + 1);
                     }
                     if (k != -1) {
-                        dp[i][j][0] = min(dp[i][j][0], pres2[k][1]);
+                        dp[i][j][0] = min(dp[i][j][0], pres2[k]);
                     }
-                    /*
-                    for (int k = 0; k <= j; ++k) {
-                        
-                        if (arr2[k] < arr2[j] && k < j) {
-                            dp[i][j][1] = min(dp[i][j][1], dp[i-1][k][1] + 1);
-                        }
-                        if (arr2[k] < arr1[i]) {
-                            dp[i][j][0] = min(dp[i][j][0], dp[i-1][k][1]);
-                        }
-                    }
-                    */
                 }
                 if (j == 0) {
-                    pres[j] = dp[i][j];
+                    pres[j] = dp[i][j][1];
                 } else {
-                    pres[j][0] = min(dp[i][j][0], pres[j-1][0]);
-                    pres[j][1] = min(dp[i][j][1], pres[j-1][1]);
+                    pres[j] = min(dp[i][j][1], pres[j-1]);
                 }
                 if (i + 1 == n) {
                     ans = min(ans, min(dp[i][j][0], dp[i][j][1]));
