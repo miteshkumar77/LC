e6ffcd0b6c2bcf2b1a377fe0ddcd5436a724fa34
miteshkumar77 <48740739+miteshkumar77@users.noreply.github.com>
2022-08-28 13:49:05 -0400

    Time: 625 ms (5.20%), Space: 119.9 MB (12.54%) - LeetHub

--- 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
+++ 1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/1866-number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -1,84 +1,27 @@
 class Solution {
 public:
-    using ll = long long;
-    
-    /*
-        A, B, M
-        
-        x % M
-        x = M * d + r, 0 <= r < M, floor(M/x) = d
-        x % M = r
-        
-        
-        (A + B) % M
-        
-        (A + B) = M * d + r
-        (A - ra) + (B - rb) = M * da + M * db
-        A + B = (M * da + ra) + (M * db + + rb)
-        
-        (A + B) % M = (A % M + B % M) % M
-    */
-    
-    ll M = 1e9 + 7;
-    ll tbl(ll i, ll j, unordered_map<int, unordered_map<int,int>>& mp) {
-        if (i == 0) { // 1 stick placed
-            if (j == 1) { // 1 visible stick
-                return 1;
-            } else { // =/= 1 visible stick
-                return 0;
-            }
-        }
-        if (j > (i + 1)) { // visible sticks > number of sticks (impossible)
+    typedef unsigned long long int ulli;
+    vector<vector<ulli>> dp;
+    ulli M = 1e9 + 7;
+    ulli dfs(ulli n, ulli k) {
+        if (n == 0 && k == 0) {
+            return 1;
+        } else if (n == 0 || k == 0) {
             return 0;
+        } else {
+            if (dp[n][k] == -1) {
+                dp[n][k] = (dfs(n - 1, k - 1) + ((n-1) * dfs(n - 1, k) % M)) % M;
+            }
+            return dp[n][k];
         }
-        
-        if (j == 0) return 0; // 0 visible sticks (impossible)
-        
-        
-        if (!mp.count(i) || !mp[i].count(j))
-            mp[i][j] = (tbl(i - 1, j - 1, mp) % M + (i * (tbl(i - 1, j, mp) % M)) % M) % M;
-        return mp[i][j];
-        
     }
-    
-    /*
-    fib(n) = fib(n-1) + fib(n-2)
-    
-    fib(4) = fib(3) + fib(2) = fib(2) + fib(1) + fib(2)
-    
-    */
-    
-    
     int rearrangeSticks(int n, int k) {
-        unordered_map<int, unordered_map<int, int>> mp;
-        return tbl(n-1, k, mp);
+        dp = vector<vector<ulli>>(n + 1, vector<ulli>(k + 1, -1));
+        /*
+            dp[i][j] = number of arrangements of sticks n - i + 1 ... n with 
+            j visible sticks
+            dp[0][0] = 1;
+        */
+        return dfs(n, k);
     }
 };
-
-
-
-/*
-    [ 
-      -
-      n    i = 0
-      -
-      n-1  i = 1
-      -
-      n-2 ] 3 sticks placed down in decreasing order and k visibile sticks
-      ^ ^
-      | |
-      
-    n-3
-    
-    4 sticks placed down and k+1 visible sticks in 1 way
-    4 sticks placed down and k visible sticks in 3 ways
-    
-    tbl[n][k] = answer to the problem
-    tbl[i][j] = the number of ways to arrange [i] sticks with [j] visible
-    tbl[i][j] = tbl[i-1][j-1] + (i) * tbl[i-1][j]
-    
-    if (i == 0) {
-        
-    }
-    
-*/
